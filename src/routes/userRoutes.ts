9b1926b10660207d7ac5f13b1c1c3bbc5abcf5ea
Matheus Sakuragui <matheus.sakuragui@solucx.com.br>
2023-09-05 11:33:53 -0300

    API20232-43 ✨: endpoint para cadastro de usuários.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ import UserController from "../controllers/userController";
 const userRouter = Router();
 
 
+userRouter.post("/create", UserController.createUser);
 userRouter.get("/getAll", UserController.getAllUsers);
 
 export default userRouter;
