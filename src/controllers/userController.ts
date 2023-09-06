c21d8469b7fdee8ef33945a6c77a81510dfd1f2e
Nathan Truyts <nathtruyts99@gmail.com>
2023-09-05 23:59:45 -0300

    API20232-43 ✨: endpoint para atualizar usuarios

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -21,7 +21,7 @@ class UserController {
             }
 
             const user: User = req.body;
-            let createNewUser = await userService.createUser(user);
+            let createNewUser = await UserService.createUser(user);
             return res.status(201).json({ message: "User created successfully", data: createNewUser });
         } catch (error) {
             res.status(500).json({ error: "Internal Server Error" });
@@ -34,7 +34,7 @@ class UserController {
         try {
             const users = await UserService.getAllUser();
             console.log(users);
-            const users = await userService.getAllUser();
+            const users = await UserService.getAllUser();
             if (!users) {
                 res.status(404).json({ error: "Users not found" });
             } else {
