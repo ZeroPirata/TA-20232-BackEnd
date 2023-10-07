fd21a4c29b664cb5f8e49e9f4e8f3f05055ccbba
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-10-07 00:04:01 -0300

    ⚙️: Verificação de arquivo de certificado na criação do banco de dados

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,27 +1,38 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
-import{
-    Task,
-    Subtask,
-    User
-} from "../models";
-
+import {DataSource} from "typeorm";
+import {Task, Subtask, User} from "../models";
 import * as dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-export const DataBaseSource = new DataSource({
+const entidades = [Task, Subtask, User];
 
-    type: "mysql",
-    host: process.env.HOST,
-    port: Number(process.env.PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-    synchronize: true,
-    logging: false, 
-    entities: [Task, Subtask, User]
+export const DataBaseSource = process.env.PATH_PEM
+  ? new DataSource({
+      type: "mysql",
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: true,
+      logging: false,
+      ssl: {
+        ca: fs.readFileSync(process.env.PATH_PEM, {encoding: "utf-8"}),
+      },
+      entities: entidades,
+    })
+  : new DataSource({
+      type: "mysql",
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: true,
+      logging: false,
+      entities: entidades,
+    });
 
-});
-
-export { DataSource };
+export {DataSource};
