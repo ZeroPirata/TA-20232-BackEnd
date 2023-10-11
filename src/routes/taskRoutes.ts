3bcbaef443a4901e3a9fa10089cfa9d2dd19f031
Nathan Truyts <nathtruyts99@gmail.com>
2023-10-11 16:51:01 -0300

    🐛: Correção na rota do tempo gasto mensalmente

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -8,7 +8,8 @@ taskRouter.get("/all", TaskController.getAllTasks);
 taskRouter.get("/getById/:id", TaskController.getTaskById);
 taskRouter.get("/getExpiredTasks/:id/:date", TaskController.getExpiredTasks);
 taskRouter.get("/getByUserId/:userId", TaskController.getTasksByUserId);
-taskRouter.get("/getTimeSpentbyMonth/:id/:month", TaskController.getTimeSpentByMonth)
+taskRouter.get("/getTimeSpentbyMonth/:id/:month", TaskController.getTimeSpentByMonth);
+taskRouter.get("/getTimeSpentMonthly/:id/:year", TaskController.getTimeSpentMonthly);
 taskRouter.put("/update/:id", TaskController.updateTask);
 taskRouter.put("/updateTime/:id", TaskController.updatetaskTimeSpent);
 taskRouter.delete("/delete/:id", TaskController.deleteTask);
