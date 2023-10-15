b676f34e8913cb1a44f75909a1c025d210761ecc
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-10-15 00:34:35 -0300

    ⚙️: Adição da tag para o swagger

--- src/routes/statusRoutes.ts
+++ src/routes/statusRoutes.ts
@@ -3,7 +3,9 @@ import { statusController } from "../controllers";
 
 const statusRouter = Router();
 
-statusRouter.get("/status", statusController.getStatus);
+statusRouter.get("/status", statusController.getStatus,
+    // #swagger.tags = ['Status']
+);
 // statusRouter.get("/timeUpdate/:id", statusController.timeUpdate);
 
 export default statusRouter;
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,18 +3,42 @@ import { TaskController } from "../controllers";
 
 const taskRouter = Router();
 
-taskRouter.post("/create", TaskController.createTask);
-taskRouter.get("/getExpiredTasks/:id/:date", TaskController.getExpiredTasks);
-taskRouter.get("/getTimeSpentbyMonth/:id/:month", TaskController.getTimeSpentByMonth);
-taskRouter.get("/getTimeSpentMonthly/:id/:year", TaskController.getTimeSpentMonthly);
-taskRouter.put("/update/:id", TaskController.updateTask);
-taskRouter.put("/updateTime/:id", TaskController.updatetaskTimeSpent);
-taskRouter.delete("/delete/:id", TaskController.deleteTask);
-taskRouter.get("/repeatTask/:id", TaskController.repeatTask);
-taskRouter.get("/getNonCyclicTaskByUserId/:id", TaskController.getAllNonCyclicTasks);
-taskRouter.get("/getById/:id", TaskController.getTaskById);
-taskRouter.get("/all", TaskController.getAllTasks);
-taskRouter.get("/getByUserId/:userId", TaskController.getTasksByUserId);
+taskRouter.post("/create", TaskController.createTask,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getExpiredTasks/:id/:date", TaskController.getExpiredTasks,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getTimeSpentbyMonth/:id/:month", TaskController.getTimeSpentByMonth,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getTimeSpentMonthly/:id/:year", TaskController.getTimeSpentMonthly,
+    // #swagger.tags = ['Task']
+);
+taskRouter.put("/update/:id", TaskController.updateTask,
+    // #swagger.tags = ['Task']
+);
+taskRouter.put("/updateTime/:id", TaskController.updatetaskTimeSpent,
+    // #swagger.tags = ['Task']
+);
+taskRouter.delete("/delete/:id", TaskController.deleteTask,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/repeatTask/:id", TaskController.repeatTask,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getNonCyclicTaskByUserId/:id", TaskController.getAllNonCyclicTasks,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getById/:id", TaskController.getTaskById,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/all", TaskController.getAllTasks,
+    // #swagger.tags = ['Task']
+);
+taskRouter.get("/getByUserId/:userId", TaskController.getTasksByUserId,
+    // #swagger.tags = ['Task']
+);
 
 
 
