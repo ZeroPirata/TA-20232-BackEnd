31d923564d6bceedb4fe09038e1ea0e1739b65cf
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-10-30 21:24:06 -0300

    ⚙️: Teste de Deploy com Azure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,19 @@
 module.exports = {
     apps: [
       {
-        name: "oracle-backend",
+        name: "oracle-backend-develop",
+        script: "ts-node",
+        args: "--transpile-only /home/azureuser/API-2023.2-Back-End/src/index.ts --env /home/azureuser/API-2023.2-Back-End/.env",
+        cwd: "/home/azureuser/API-2023.2-Back-End/",
+        instances: 1,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: "1G",
+        output: "/dev/null",
+        error: "/dev/null", 
+      },
+      {
+        name: "oracle-backend-production",
         script: "ts-node",
         args: "--transpile-only /home/azureuser/API-2023.2-Back-End/src/index.ts --env /home/azureuser/API-2023.2-Back-End/.env",
         cwd: "/home/azureuser/API-2023.2-Back-End/",
